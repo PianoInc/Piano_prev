a4c8e204c34e52f1dd03260973463caf72e101a4
Kevin <Kevin_baba@naver.com>
2018-05-29 19:44:11 +0900

    string에서 font와 width를 받아 첫줄 계산하도록 구현

--- Piano/NoteListViewController/DeletedNoteCell.swift
+++ Piano/NoteListViewController/DeletedNoteCell.swift
@@ -19,8 +19,9 @@ class DeletedNoteCell: UICollectionViewCell, CollectionDataAcceptable {
             guard let data = self.data as? Note else { return }
             footnoteLabel.text = data.footnote
             shareImageView.isHidden = !data.type.isShared
-            titleLabel.text = data.title
-            subTitleLabel.text = data.subTitle
+            let firstLineText = data.content.firstLineText(font: titleLabel.font, width: titleLabel.bounds.width)
+            titleLabel.text = firstLineText
+            subTitleLabel.text = data.content.sub(firstLineText.count...)
         }
     }
     
