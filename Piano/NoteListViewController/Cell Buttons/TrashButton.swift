fd61a64b27804b2a48c54ba007b88480576687c8
Kevin <Kevin_baba@naver.com>
2018-06-07 12:52:33 +0900

    노트 셀 뒤에 존재하는 버튼 UI (휴지통, 고정, 잠금, 잠금해제, 복원, 카테고리이동) 작업

---
+++ Piano/NoteListViewController/Cell Buttons/TrashButton.swift
@@ -0,0 +1,21 @@
+//
+//  TrashButton.swift
+//  Piano
+//
+//  Created by Kevin Kim on 2018. 6. 5..
+//  Copyright © 2018년 Piano. All rights reserved.
+//
+
+import UIKit
+
+class TrashButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
