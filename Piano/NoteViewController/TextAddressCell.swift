0be8ff6a0538be42711ba929787e92c877d5721b
Kevin <Kevin_baba@naver.com>
2018-06-08 15:55:58 +0900

    카드 셀 추가

---
+++ Piano/NoteViewController/TextAddressCell.swift
@@ -0,0 +1,21 @@
+//
+//  TextAddressCell.swift
+//  Piano
+//
+//  Created by Kevin Kim on 08/06/2018.
+//  Copyright © 2018 Piano. All rights reserved.
+//
+
+import UIKit
+
+class TextAddressCell: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Piano/NoteViewController/TextEventCell.swift
@@ -0,0 +1,21 @@
+//
+//  TextEventCell.swift
+//  Piano
+//
+//  Created by Kevin Kim on 07/06/2018.
+//  Copyright © 2018 Piano. All rights reserved.
+//
+
+import UIKit
+
+class TextEventCell: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
